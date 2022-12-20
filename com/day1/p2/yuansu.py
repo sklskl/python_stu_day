a75ffb55c640cf06f9111c9228628c34b5ed892f
sunkeling <454541398@qq.com>
2022-12-20 16:32:34 +0800

    2.2saving

--- com/day1/p2/yuansu.py
+++ com/day1/p2/yuansu.py
@@ -3,7 +3,7 @@ if num % 2 == 0:
     if num % 5 == 0:
         print('你输入的数可以被2和5整除')
     print('你输入的数可以被2整除')
-elif num % 5 ==0:
+elif num % 5 == 0:
     print('你输入的数可以被5整除')
 else:
     print('你输入的数不可以被2和5整除,')
