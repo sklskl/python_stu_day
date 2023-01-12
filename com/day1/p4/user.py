3fc18058e4481afa75ac6b7e4065167646eadba7
sunkeling <454541398@qq.com>
2023-01-13 00:12:33 +0800

    程序结束

--- com/day1/p4/user.py
+++ com/day1/p4/user.py
@@ -1,3 +1,32 @@
+class people:
+    name = ''
+    sex = 0
+    __height = 0
+
+    def __init__(self, n, a, w):
+        self.name = n
+        self.sex = a
+        self.__height = w
+
+    def speak(self):
+        print("%s说我的性别是%s。".format(self.name, self.sex))
+
+
+class son(people):
+    grade = ''
+
+    def __init__(self, n, a, w, g):
+        people.__init__(self, n, a, w)
+        self.grade = g
+
+    def speak(self):
+        print("{}说我的性别是{},我在读{}年纪。".format(self.name, self.sex, self.grade))
+
+
+s = son('skl', '女', 12, 6)
+s.speak()
+
+
 # def user(your_name):
 #     name = your_name
 #     print('hello,' + name)
